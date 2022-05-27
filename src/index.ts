c13a7ce73511f5821dcfa4b1c63ed0e2af6268db
Blake <blakestacks@rocketmail.com>
2022-05-27 10:31:54 -0700

    generated docs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
+/** @module @superflag/client-react */
+
 export * from './types';
 export * from './hooks';
 export * from './components';
 export { DefaultFeatureFlagCacheKey } from './constants';
-export { decodeBase64 } from './utils/helpers';
-export { encodeBase64 } from './utils/helpers';
